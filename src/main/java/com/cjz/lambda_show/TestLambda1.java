c1077166650c8d6822498715e4b04e8c09e5bcb0
ChenJingZhuo <3279838729@qq.com>
2020-10-24 22:53:37 +0800

    lambda表达式

---
+++ src/main/java/com/cjz/lambda_show/TestLambda1.java
@@ -0,0 +1,54 @@
+package com.cjz.lambda_show;
+
+public class TestLambda1 {
+
+    //静态内部类
+    static class ILike implements Like{
+        @Override
+        public void like() {
+            System.out.println("I like you 2");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        //局部内部类
+        class ILike implements Like{
+            @Override
+            public void like() {
+                System.out.println("I like you 3");
+            }
+        }
+
+        //匿名内部类
+        Like like = new Like() {
+            @Override
+            public void like() {
+                System.out.println("I like you 4");
+            }
+        };
+        like.like();
+
+        Like like5 = ()-> {
+            System.out.println("I like you 5");
+        };
+        like5.like();
+
+        Like like6 = ()-> System.out.println("I like you 6");
+        like6.like();
+    }
+
+}
+
+//函数式接口：当一个接口只有一个抽象方法时，这个接口就是函数式接口。
+interface Like{
+    void like();
+}
+
+//外部类
+class ILike implements Like{
+    @Override
+    public void like() {
+        System.out.println("I like you 1");
+    }
+}
---
+++ src/main/java/com/cjz/lambda_show/TestLambda2.java
@@ -0,0 +1,27 @@
+package com.cjz.lambda_show;
+
+public class TestLambda2 {
+    public static void main(String[] args) {
+        /*Love love = new Love() {
+            @Override
+            public void love(int a, int b) {
+                System.out.println("a="+a+", b="+b);
+            }
+        };*/
+
+        /*Love love = (int a, int b) -> {
+            System.out.println("a="+a+", b="+b);
+        };*/
+
+        //Love love = () -> System.out.println("xxx");
+        //Love love = a -> System.out.println("a="+a+");
+
+        Love love = (int a, int b) -> System.out.println("a="+a+", b="+b);
+
+        love.love(520,521);
+    }
+}
+
+interface Love{
+    void love(int a, int b);
+}
